9de76e39dbef4af9041499078e5b3c4c927e6ef6
Sam Singhania <sam.r.us98@gmail.com>
2022-03-03 01:55:37 +0000

    testing chloropeth map

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px  # (version 4.7.0 or higher)
 import plotly.graph_objects as go
+import dash
 from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0.0 or higher)
 
 
